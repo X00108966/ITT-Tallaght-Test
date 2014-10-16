a24ddcbd394f278bc10f7699c870193231633221
x00108966 <gareth1996lfc@gmail.com>
2014-10-16 10:57:35 +0100

    welcome to the land of Nazareth

---
+++ x00108966-grades.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Exam 49  Practical 49"
+java grades 49 49
+echo "Exam B Practical 9"
+java grades B 9
+echo "Exam 101 Practical 98"
+java grades 101 98
+echo "Exam 41 Practical 41"
+java grades 41 41
+echo "Exam 79 Practical 78"
+java grades 79 78
+echo "Exam 81 Practical 82"
+java grades 81 82
